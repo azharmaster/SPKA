ad3afbfe70dd987df0abbfe59432d87a3c64d085
MasterCode <niazhar96@gmail.com>
2025-02-17 09:09:29 +0800

    .

--- inc/header.php
+++ inc/header.php
@@ -11,12 +11,18 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link font-weight">SISTEM PENGAWASAN KEBAKARAN AUTOMATIK</a>
+      </li>
+    </ul>
+    
+    <!-- <ul class="navbar-nav">
+      <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
       </li>
-    </ul>
+    </ul> -->
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
